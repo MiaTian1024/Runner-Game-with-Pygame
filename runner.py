d89c16d290bf917aaea7457b4ec864c172834712
MiaTian1024 <sddxtm123@gmail.com>
2023-07-01 13:23:04 +1200

    succeed render a snail

--- runner.py
+++ runner.py
@@ -5,9 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+test_surface = test_font.render('My game', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -17,5 +22,9 @@ while True:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(test_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 270))
     pygame.display.update()
     clock.tick(60)
