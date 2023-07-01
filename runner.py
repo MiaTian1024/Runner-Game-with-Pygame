f2a8542cca88ad4e4a48cfa35e4dd0ec65712a5e
MiaTian1024 <sddxtm123@gmail.com>
2023-07-01 14:14:49 +1200

    update colors

--- runner.py
+++ runner.py
@@ -5,27 +5,44 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-test_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+score_surf = text_font.render('My game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400, 50))
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     print(event.pos)
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(test_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 270))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+   
+    screen.blit(score_surf, score_rect)
+    
+    snail_rect.x -= 4
+    if snail_rect.right <= 0 : snail_rect.left =800
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    
+    # player_rect.collidepoint(())
 
     pygame.display.update()
     clock.tick(60)
